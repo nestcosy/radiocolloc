c4011f9fd7b605e5619ef954310d6a537d304995
bug87 <bug87@hotmail.fr>
2025-12-05 15:59:38 +0100

    o

--- app/manage.py
+++ app/manage.py
@@ -16,8 +16,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-    os.printf("SALUTUTUUTUTUTU")
-    printf('SaLUUUTUUTUTUT')
 
 
 if __name__ == '__main__':
